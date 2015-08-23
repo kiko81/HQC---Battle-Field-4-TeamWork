41b3eb73350e54bb9c896988d4d47de2afd0ac56
kiko81 <k.kolev@tobo-bg.com>
2015-08-23 14:20:26 +0300

    Work directory BattleField added - extracted mine classes along with abstract mine class

---
+++ BattleField/GameObjects/Mine.cs
@@ -0,0 +1,15 @@
+﻿namespace BattleField.GameObjects
+{
+    public abstract class Mine
+    {
+        public int[,] Explosion
+        {
+            get
+            {
+                return this.Explode();
+            }
+        }
+
+        protected abstract int[,] Explode();
+    }
+}
---
+++ BattleField/GameObjects/SingleMine.cs
@@ -0,0 +1,18 @@
+﻿namespace BattleField.GameObjects
+{
+    public class SingleMine : Mine
+    {
+        private readonly int[,] mine =
+        {
+            {0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0}
+        };
+        protected override int[,] Explode()
+        {
+            return this.mine;
+        }
+    }
+}
