fcaa67f47ebdf8704519c1f9856b0d566fb9eb64
kiko81 <k.kolev@tobo-bg.com>
2015-09-15 16:45:01 +0300

    rearranging folders according to requirements

---
+++ src/BattleField/GameObjects/QuadMine.cs
@@ -0,0 +1,18 @@
+﻿namespace BattleField.GameObjects
+{
+    public class QuadMine : Mine
+    {
+        private readonly int[,] mine =
+        {
+            {0, 1, 1, 1, 0},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 0}
+        };
+        protected override int[,] Explode()
+        {
+            return this.mine;
+        }
+    }
+}
---
+++ src/BattleField/GameObjects/QuintMine.cs
@@ -0,0 +1,18 @@
+﻿namespace BattleField.GameObjects
+{
+    public class QuintMine : Mine
+    {
+        private readonly int[,] mine =
+        {
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1}
+        };
+        protected override int[,] Explode()
+        {
+            return this.mine;
+        }
+    }
+}
