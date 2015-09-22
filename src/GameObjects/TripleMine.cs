ee2545b2a06f0ffac0dd21f011cc0ea4e6b8a1f5
Petar Zubev <p-zubev@operamail.com>
2015-09-22 13:08:15 +0300

    Put using under namespace in TripleMine.cs

--- src/GameObjects/TripleMine.cs
+++ src/GameObjects/TripleMine.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace BattleField.GameObjects
+﻿namespace BattleField.GameObjects
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public class TripleMine : Mine
     {
         private readonly int[,] mine =
