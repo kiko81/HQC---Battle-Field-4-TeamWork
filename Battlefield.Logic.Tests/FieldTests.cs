67eb43b6eaa11243a05823b96e0972dbf80ea70e
Petar Zubev <p-zubev@operamail.com>
2015-10-09 23:51:49 +0300

    Creating Battlefield.Logic.Tests project

---
+++ Battlefield.Logic.Tests/FieldTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Battlefield.Logic.Tests
+{
+    [TestClass]
+    public class FieldTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
